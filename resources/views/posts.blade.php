4ff1f0741e4b43a6ecd50c4aac991efeb4dc203a
Voufack Harold <voufackharold@gmail.com>
2023-04-18 00:04:41 +0100

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    
+    <article>
+        <h1><a href="posts/my-first-post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac urna libero. Sed vel ex pellentesque, dictum orci id, rhoncus lectus. 
+            Vestibulum nibh enim, ornare quis pellentesque eget, tincidunt eget elit. Mauris at enim efficitur ex iaculis efficitur.
+            Duis vel ullamcorper tellus. Sed et tortor erat. Nullam cursus orci non urna vestibulum ornare. Ut sodales sem vitae congue efficitur.
+            Vivamus eu neque felis. Nam non mattis dolor. Nunc consequat iaculis sem. Nunc elementum dolor quam, vel lacinia sem dignissim vitae.
+            Vestibulum finibus, dui ac semper tincidunt, metus tortor faucibus tellus, sit amet tincidunt eros felis mollis sem.
+            Nunc non vestibulum elit. Phasellus vehicula erat non lacinia eleifend. Suspendisse placerat pretium pulvinar. 
+            Pellentesque sed sapien lobortis, porta elit id, consequat ante.
+        </P>
+    </article>
+
+
+    <article>
+        <h1><a href="posts/my-second-post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac urna libero. Sed vel ex pellentesque, dictum orci id, rhoncus lectus. 
+            Vestibulum nibh enim, ornare quis pellentesque eget, tincidunt eget elit. Mauris at enim efficitur ex iaculis efficitur.
+            Duis vel ullamcorper tellus. Sed et tortor erat. Nullam cursus orci non urna vestibulum ornare. Ut sodales sem vitae congue efficitur.
+            Vivamus eu neque felis. Nam non mattis dolor. Nunc consequat iaculis sem. Nunc elementum dolor quam, vel lacinia sem dignissim vitae.
+            Vestibulum finibus, dui ac semper tincidunt, metus tortor faucibus tellus, sit amet tincidunt eros felis mollis sem.
+            Nunc non vestibulum elit. Phasellus vehicula erat non lacinia eleifend. Suspendisse placerat pretium pulvinar. 
+            Pellentesque sed sapien lobortis, porta elit id, consequat ante.
+        </P>
+    </article>
+
+    <article>
+        <h1><a href="posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac urna libero. Sed vel ex pellentesque, dictum orci id, rhoncus lectus. 
+            Vestibulum nibh enim, ornare quis pellentesque eget, tincidunt eget elit. Mauris at enim efficitur ex iaculis efficitur.
+            Duis vel ullamcorper tellus. Sed et tortor erat. Nullam cursus orci non urna vestibulum ornare. Ut sodales sem vitae congue efficitur.
+            Vivamus eu neque felis. Nam non mattis dolor. Nunc consequat iaculis sem. Nunc elementum dolor quam, vel lacinia sem dignissim vitae.
+            Vestibulum finibus, dui ac semper tincidunt, metus tortor faucibus tellus, sit amet tincidunt eros felis mollis sem.
+            Nunc non vestibulum elit. Phasellus vehicula erat non lacinia eleifend. Suspendisse placerat pretium pulvinar. 
+            Pellentesque sed sapien lobortis, porta elit id, consequat ante.
+        </P>
+    </article>
+
+</body>
